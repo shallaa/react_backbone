dc2c93597bce143ea65223e2b93d1f59f4a49076
shallaa <shallaa@shallaazm.com>
2015-11-02 16:09:25 +0900

    mixin을 좀더 세분화하여 처리하는 것으로..

--- app/core/component-mixin.js
+++ app/core/component-mixin.js
@@ -1,12 +1,18 @@
 module.exports = {
-  componentDidMount: function() {
-    this.getBackboneCollections().forEach(function(collection) {
-      collection.on('add remove change', this.forceUpdate.bind(this, null));
-    }, this);
+  componentDidMount: function () {
+    // TODO: 기본적인 초기 셋팅
+    // listenTo, ...
+
+    //this.getBackboneCollections().forEach(function (collection) {
+    //  collection.on('add remove change', this.forceUpdate.bind(this, null));
+    //}, this);
   },
-  componentWillUnmount: function() {
-    this.getBackboneCollections().forEach(function(collection) {
-      collection.off(null, null, this);
-    }, this);
+  componentWillUnmount: function () {
+    // TODO: 기본적인 삭제
+    // remove, ...
+    
+    //this.getBackboneCollections().forEach(function (collection) {
+    //  collection.off(null, null, this);
+    //}, this);
   }
 };
